abfb57f466acef302babab4dace0e97f572bcbb8
Cristian Andres Picon Rodriguez <picon@mintsoftware.net>
2024-09-29 12:12:03 +0200

    Structuring the package API · Issue #1
    view arguments model moved

--- lib/src/fx_navigation/models/fx_navigation_models.dart
+++ lib/src/fx_navigation/models/fx_navigation_models.dart
@@ -1,3 +1,2 @@
 export 'navigation_arguments.dart';
-export 'view_arguments.dart';
 export 'enums.dart';
--- lib/src/fx_suit/fx_suite.dart
+++ lib/src/fx_suit/fx_suite.dart
@@ -3,3 +3,4 @@ library fx_suit;
 export 'fx_view.dart';
 export 'fx_extensions.dart';
 export 'fx_components.dart';
+export './models/fx_suite_models.dart';
